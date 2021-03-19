cfc19fe9afc506f93136389c5877057148c40e43
REVEA <seris12600952@gmai.com>
2021-03-20 00:49:02 +0900

    Initial Commit

---
+++ Hello,Swift!/Hello_Swift_App.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_Swift_App.swift
+//  Hello,Swift!
+//
+//  Created by ReDead on 2021/03/20.
+//
+
+import SwiftUI
+
+@main
+struct Hello_Swift_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
