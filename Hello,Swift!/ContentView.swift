a3c82d34871543d7b70a9c51f5c523554d70fa0e
REVEA <seris12600952@gmai.com>
2021-04-13 22:24:58 +0900

    Hello,Swift

--- Hello,Swift!/ContentView.swift
+++ Hello,Swift!/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var outputText="Hello World!"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            Text(outputText)
+                .font(.largeTitle)
+                .padding()
+            
+            Button(action: {
+                outputText="Hi, Swift!"
+                print("デバックテスト")
+                print(outputText)
+                
+            }) {
+                Text("切り替えボタン")
+                    .foregroundColor(Color.white)
+                    .padding(.all)
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+            }
+        }
+        
+        
     }
 }
 
